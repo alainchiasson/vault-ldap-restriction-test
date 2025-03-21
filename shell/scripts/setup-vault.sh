d54231121a7f4ac19cacae2c8288d92d2dfa54af
Alain Chiasson <alain@chiasson.org>
2025-03-21 06:58:57 -0400

    Fixed bugs

--- shell/scripts/setup-vault.sh
+++ shell/scripts/setup-vault.sh
@@ -11,43 +11,48 @@ vault login root
 # Enable ldap auth
 vault auth enable ldap
 
-# # Login only people in vault group
-# vault write auth/ldap/config url="${LDAP_ADDR}" \
-#   bindpass="admin" \
-#   starttls=false \
-#   userdn="ou=users,dc=example,dc=org" \
-#   groupdn="ou=groups,dc=example,dc=org" \
-#   binddn="cn=admin,dc=example,dc=org" \
-#   userfilter="(&(objectClass=Person)({{.UserAttr}}={{.Username}})(memberOf=cn=vault,ou=groups,dc=example,dc=org))" ;
-#   # Success! Data written to: auth/ldap/config
-
-
-# LDAP Auth config that uses your own user/pass.
+# Login only people in vault group - uses admin wihtout password
 vault write auth/ldap/config url="${LDAP_ADDR}" \
+  bindpass="admin" \
   starttls=false \
-  userattr="CN" \
   userdn="ou=users,dc=example,dc=org" \
   groupdn="ou=groups,dc=example,dc=org" \
   binddn="cn=admin,dc=example,dc=org" \
   userfilter="(&(objectClass=Person)({{.UserAttr}}={{.Username}})(memberOf=cn=vault,ou=groups,dc=example,dc=org))" ;
+  # Success! Data written to: auth/ldap/config
 
 
-# Add User Policies
-vault policy write mng_team_secrets - <<EOF
-path "team-secrets/data/" {
+# # LDAP Auth config that uses your own user/pass.
+# vault write auth/ldap/config url="${LDAP_ADDR}" \
+#   starttls=false \
+#   userattr="CN" \
+#   userdn="ou=users,dc=example,dc=org" \
+#   groupdn="ou=groups,dc=example,dc=org" \
+#   userfilter="(&(objectClass=Person)({{.UserAttr}}={{.Username}})(memberOf=cn=vault,ou=groups,dc=example,dc=org))" ;
+
+
+for user in 1 2 4 5 ; do
+
+    vault auth list -format=json | jq -r '.["ldap/"].accessor' > ldap_accessor.txt
+    vault write -format=json identity/entity name="user$user" policies="user_secrets" metadata=type="user" | jq -r ".data.id" > entity_id.txt
+    vault write identity/entity-alias name="user$user" canonical_id=$(cat entity_id.txt) mount_accessor=$(cat ldap_accessor.txt)
+
+done
+
+vault policy write user_secrets - <<EOF
+path "{{identity.entity.name}}/data/" {
     capabilities = ["list", "read", "create", "update", "delete"]
 }
-path "team-secrets/data/*" {
+path "{{identity.entity.name}}/data/*" {
     capabilities = ["list", "read", "create", "update", "delete"]
 }
-path "team-secrets/metadata/" {
+path "{{identity.entity.name}}/metadata/" {
     capabilities = ["list", "read", "create", "update", "delete"]
 }
-path "team-secrets/metadata/*" {
+path "{{identity.entity.name}}/metadata/*" {
     capabilities = ["list", "read", "create", "update", "delete"]
 }
 EOF
-
 
 vault policy write dev_secrets - <<EOF
 path "team-secrets/data/{{identity.groups.names.dev.id}}/*" {
--- shell/scripts/test-setup.sh
+++ shell/scripts/test-setup.sh
@@ -4,6 +4,5 @@
 for x in {1..6} ; do
   echo "===> Logging in as user${x}" ;
   vault login -method=ldap username=user${x} password=user${x} ;
-  vault read -format=json sys/internal/ui/resultant-acl | jq -r '..data.glob_paths' ;
-
+  vault read -format=json sys/internal/ui/resultant-acl | jq -r '.data.glob_paths' ;
 done
